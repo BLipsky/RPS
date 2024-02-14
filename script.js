ea57c31da3e09f24b145227e35deee16dbd75a23
Ben_Lipsky <Benlipsky@icloud.com>
2024-02-14 13:48:58 -0500

    New

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-function display(){
 
-let difOne = '...'
-let difTwo = '...'
-let difThree = '...'
+let disName 
+let disComName
 
-function difOne(){
-    
-    if (difOne === 'Easy'){
-        return 'You Choose Easy'
-    } else if (difOne === 'Normal'){
-        return 'You Choose Normal'
-    } else if (difOne === 'Hard'){
-        return 'You Choose Hard'
-    }
-    
+function display() {
+
+    disComName = document.getElementById('comName').value
+    document.getElementById('displayComputerName').textContent = disComName
+
+    disName = document.getElementById('plaName').value
+    document.getElementById('displayPlayerName').textContent = disName
+
+
+    document.getElementById('fullOne').classList.add("d-none")
 }
+
 
 let responseOneArray = [
     'Rock',
@@ -26,7 +25,35 @@ let responseOneArray = [
     'Scissors'
     ]
     
-    let randomNumber = Math.floor(Math.random()* responseOneArray.length); // Generating Press Random
+
+
+
+
+ //   let randomNumber = Math.floor(Math.random()* responseOneArray.length); // Generating Press Random
     
-    document.getElementById('press-quote').textContent = responseOneArray[randomNumber]
-}
+ //   document.getElementById('answer').textContent = responseOneArray[randomNumber]
+//    console.log(disName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function selectOne(){
+    
+// if (difSelector === 'Easy'){
+//    return 'You Choose Easy'
+// } else if (difSelector === 'Normal'){
+//    return 'You Choose Normal'
+// } else if (difSelector === 'Hard'){
+//    return 'You Choose Hard'
+// }
+// } 
