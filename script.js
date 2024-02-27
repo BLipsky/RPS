92e1e21451c5f3c28c3f4efa35b8dbeb97078c44
Ben_Lipsky <Benlipsky@icloud.com>
2024-02-27 14:52:58 -0500

    vome

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 let disName;
 let disComName;
 document.getElementById("gameDiv").classList.add("d-none");
+document.getElementById("new").classList.add("d-none");
 let computerValue = 0;
 
 function display() {
   disComName = document.getElementById("comName").value;
-  document.getElementById("displayComputerName").textContent = "Away Team";
+  document.getElementById("displayComputerName").textContent = "CPU:";
   document.getElementById("displayComputerName1").textContent = disComName;
 
   disName = document.getElementById("plaName").value;
-  document.getElementById("displayPlayerName").textContent = "Home Team";
+  document.getElementById("displayPlayerName").textContent = "Player:";
   document.getElementById("displayPlayerName1").textContent = disName;
 
+  document.getElementById("body").classList.add("dope");
   document.getElementById("fullOne").classList.add("d-none");
   document.getElementById("gameDiv").classList.remove("d-none");
+  document.getElementById("new").classList.remove("d-none");
+
 }
 function responseNew() {
   let humanRes = document.getElementById("humanRes").value;
   computerValue++;
   document.getElementById("displayScoreName").textContent = computerValue;
+  document.getElementById("bot").classList.add("bot2");
 
-  if (humanRes === "Run") {
-    return "Fumble!";
-  } else if (humanRes === "Pass") {
-    return "Interception!";
-  } else if (humanRes === "Play Action") {
-    return "Turn Over on Downs!";
+  if (humanRes === "Rock") {
+    return "Paper";
+  } else if (humanRes === "Paper") {
+    return "Scissors";
+  } else if (humanRes === "Scissors") {
+    return "Rock";
   } else {
     return "No Answer";
   }
@@ -39,7 +44,11 @@ document.getElementById("lockInButton").addEventListener("click", function () {
     document.getElementById("gameOver").textContent = "Game Over!";
     document.getElementById("humanRes").classList.add("d-none");
     document.getElementById("lockInButton").classList.add("d-none");
-    document.getElementById("gameOver").classList.add("done"); 
+    document.getElementById("gameOver").classList.add("done");
+    document.getElementById("new").classList.remove("new");
+    document.getElementById("new").classList.add("new2");
+    document.getElementById("resTwo").classList.add("d-none");
+
   }
 });
 
