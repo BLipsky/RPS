7eaa0b74e2bba938d459dd079d9d5c7cbaffd919
Ben_Lipsky <Benlipsky@icloud.com>
2024-03-01 13:20:31 -0500

    Sewers

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//Define Early
+
 let disName;
 let disComName;
 document.getElementById("gameDiv").classList.add("d-none");
@@ -5,12 +8,13 @@ document.getElementById("new").classList.add("d-none");
 document.getElementById("try").classList.add("d-none");
 let computerValue = 0;
 let humanValue = 0;
-console.log(hardOne())
+let diffPickerDrpDwn = document.querySelector("#difSelector")
 //Select Difficulty
 
 function selectOne() {
+  console.log('im here')
   if (difSelector === "difOne") {
-     easyOne();
+    easyOne();
   } else if (difSelector === "difTwo") {
     return normalOne();
   } else if (difSelector === "difThree") {
@@ -21,6 +25,8 @@ function selectOne() {
 //Display Screen 
 
 function display() {
+  difSelector = diffPickerDrpDwn.value
+
   disComName = document.getElementById("comName").value;
   document.getElementById("displayComputerName").textContent = "CPU:";
   document.getElementById("displayComputerName1").textContent = disComName;
@@ -37,85 +43,86 @@ function display() {
 
 //Easy Option
 
-function easyOne(){
-function responseNew1() {
-  let humanRes = document.getElementById("humanRes").value;
-  humanValue++;
-  document.getElementById("displayScoreName1").textContent = humanValue;
+function easyOne() {
+  function responseNew1() {
+    humanValue++
+    let humanRes = document.getElementById("humanRes").value;
+    document.getElementById("displayScoreName1").textContent = humanValue;
 
-  if (humanRes === "Rock") {
-    return "Scissors";
-  } else if (humanRes === "Paper") {
+    if (humanRes === "Rock") {
+      return "Scissors";
+    } else if (humanRes === "Paper") {
     return "Rock";
-  } else if (humanRes === "Scissors") {
-    return "Paper";
-  } else {
-    return "No Answer";
+    } else if (humanRes === "Scissors") {
+      return "Paper";
+    } else {
+      return "No Answer";
+    }
+    
   }
-}
   document.getElementById("lockInButton").addEventListener("click", function () {
-  document.getElementById("resOne").textContent = responseNew1();
-if (humanValue === 5) {
-  document.getElementById("goodGame").textContent = "Good Game!";
-  document.getElementById("humanRes").classList.add("d-none");
-  document.getElementById("lockInButton").classList.add("d-none");
-  document.getElementById("gameOver").classList.add("done");
-  document.getElementById("new").classList.remove("new");
-  document.getElementById("new").classList.add("new3");
-  document.getElementById("resTwo").classList.add("d-none");
-  document.getElementById("try").classList.remove("d-none");
-}
-});
+    document.getElementById("resOne").textContent = responseNew1();
+    if (humanValue === 5) {
+      document.getElementById("goodGame").textContent = "Good Game!";
+      document.getElementById("humanRes").classList.add("d-none");
+      document.getElementById("lockInButton").classList.add("d-none");
+      document.getElementById("gameOver").classList.add("done");
+      document.getElementById("new").classList.remove("new");
+      document.getElementById("new").classList.add("new3");
+      document.getElementById("resTwo").classList.add("d-none");
+      document.getElementById("try").classList.remove("d-none");
+    }
+  });
 }
 
 //Hard Option
 
 function hardOne() {
-function responseNew() {
-  let humanRes = document.getElementById("humanRes").value;
-  computerValue++;
-  document.getElementById("displayScoreName").textContent = computerValue;
+  function responseNew() {
+    let humanRes = document.getElementById("humanRes").value;
+    computerValue++;
+    document.getElementById("displayScoreName").textContent = computerValue;
 
-  if (humanRes === "Rock") {
-    return "Paper";
-  } else if (humanRes === "Paper") {
-    return "Scissors";
-  } else if (humanRes === "Scissors") {
-    return "Rock";
-  } else {
-    return "No Answer";
+    if (humanRes === "Rock") {
+      return "Paper";
+    } else if (humanRes === "Paper") {
+      return "Scissors";
+    } else if (humanRes === "Scissors") {
+      return "Rock";
+    } else {
+      return "No Answer";
+    }
   }
-}
-document.getElementById("lockInButton").addEventListener("click", function () {
-  document.getElementById("resOne").textContent = responseNew();
-  if (computerValue === 5) {
-    document.getElementById("gameOver").textContent = "Game Over!";
-    document.getElementById("humanRes").classList.add("d-none");
-    document.getElementById("lockInButton").classList.add("d-none");
-    document.getElementById("gameOver").classList.add("done");
-    document.getElementById("new").classList.remove("new");
-    document.getElementById("new").classList.add("new2");
-    document.getElementById("resTwo").classList.add("d-none");
-    document.getElementById("try").classList.remove("d-none");
-  }
-});
+  document.getElementById("lockInButton").addEventListener("click", function () {
+    document.getElementById("resOne").textContent = responseNew();
+    if (computerValue === 5) {
+      document.getElementById("gameOver").textContent = "Game Over!";
+      document.getElementById("humanRes").classList.add("d-none");
+      document.getElementById("lockInButton").classList.add("d-none");
+      document.getElementById("gameOver").classList.add("done");
+      document.getElementById("new").classList.remove("new");
+      document.getElementById("new").classList.add("new2");
+      document.getElementById("resTwo").classList.add("d-none");
+      document.getElementById("try").classList.remove("d-none");
+    }
+  });
 }
 
 //Normal Option
 
 function normalOne() {
-let responseOneArray = [
-  "Rock",
-  "Paper",
-  "Scissors",
-  "Scissors",
-  "Rock",
-  "Paper",
-  "Scissors",
-];
-let randomNumber = Math.floor(Math.random() * responseOneArray.length);
+  let responseOneArray = [
+    "Rock",
+    "Paper",
+    "Scissors",
+    "Scissors",
+    "Rock",
+    "Paper",
+    "Scissors",
+  ];
+  let randomNumber = Math.floor(Math.random() * responseOneArray.length);
 
-document.getElementById("answer").textContent = responseOneArray[randomNumber];
-console.log(disName);
+  document.getElementById("answer").textContent = responseOneArray[randomNumber];
+  console.log(disName);
 
 }
